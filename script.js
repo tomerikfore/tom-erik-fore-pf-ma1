d7e6c75d24d3c945554e1ddcb196bb43e1002990
tomerikfore <71603131+tomerikfore@users.noreply.github.com>
2020-09-20 19:44:26 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// question 1
+
+var firstVar;
+
+// question 2
+
+var name = "tom erik føre";
+
+// question 3
+
+var age = 24;
+
+// question 4
+
+var division = 20/5;
+
+// question 5
+
+typeof("frog");
+// "string"
+
+// question 6
+
